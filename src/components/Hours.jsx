d98988502ee0fad87252e266fa81711d7480162e
SeanChid <seanchidester737@gmail.com>
2023-11-29 16:39:42 -0800

    Initial Commit

---
+++ src/components/Hours.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Hours = (props) => {
+
+  const {isEditing, value, setHours} = props
+
+  return isEditing? (
+    <td>
+      <input 
+        type='text' 
+        value={value}
+        onChange={(e) => setHours(e.target.value)}
+      />
+    </td>
+  ) : (
+    <td>
+      {value}
+    </td>
+  )
+}
+
+export default Hours
